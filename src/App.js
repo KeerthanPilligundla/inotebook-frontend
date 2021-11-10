1f308f4de3f8d32b399fd7ddabf698f3e13c8c5e
KeerthanPilligundla <keerthan245@gmail.com>
2021-11-10 17:28:56 +0530

    intial

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+    </Router>
+    </>
+  );
+}
+export default App;
